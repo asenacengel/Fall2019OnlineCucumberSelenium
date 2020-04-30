d3af10ef85ed11d057db489663327468cf277908
Asena Cengel <asena.eroglu@gmail.com>
2020-04-30 14:15:58 -0400

    Cucumber

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -17,8 +17,9 @@ public class Driver {
 
     }
 
-    /**synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
-     *
+    /**
+     * synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
+     * <p>
      * Thread safety reduces performance but it makes everything safe.
      *
      * @return
@@ -29,6 +30,11 @@ public class Driver {
         if (driverPool.get() == null) {
             //specify browser type in configuration.properties file
             String browser = ConfigurationReader.getProperty("browser").toLowerCase();
+            // -Dbrowser=firefox
+            if (System.getProperty("browser") != null) {
+                browser = System.getProperty("browser");
+            }
+
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
@@ -54,8 +60,9 @@ public class Driver {
         return driverPool.get();
     }
 
-    /**synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
-     *
+    /**
+     * synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
+     * <p>
      * Thread safety reduces performance but it makes everything safe.
      *
      * @return
